db40cf85628f8c9ed7f4a606bb679d8d21c5ade7
antoinel74 <antoine.lansman@gmail.com>
2023-09-08 10:42:47 +0200

    init files

---
+++ 1.1.js
@@ -0,0 +1,13 @@
+console.log("Hello world");
+
+let name = "connard";
+let age = "26";
+
+console.log("My name is " + name + "and i got " + age); 
+
+let htmlcup = new Number('5');
+let csscup = new Number('8'); 
+let totalcup = htmlcup + csscup;
+
+console.log("Wow you got " + htmlcup + " html cup and " + csscup + " css cup, that's impressive !");
+console.log("You got " + totalcup + " cup in total !");
