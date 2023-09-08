5f7c283abf5228391a3c0326bc18e7b0939d8390
antoinel74 <antoine.lansman@gmail.com>
2023-09-08 14:54:52 +0200

    exo 2.5

---
+++ 2.5.js
@@ -0,0 +1,5 @@
+var favnumber = prompt("What is your favourite number");
+
+while (favnumber !== "42") { favnumber = prompt("Are you sure ?"); }
+
+alert("Oh yeah !! Welcome on the secret fields !");
