e09acce32212f9b0703ad5a1e906c5878ad50568
antoinel74 <antoine.lansman@gmail.com>
2023-09-11 11:55:27 +0200

    ex 3.4

---
+++ 3.4.js
@@ -0,0 +1,12 @@
+let numInput = prompt("Enter a list a numbers separated by a space :");
+var numbers = numInput.split(" ");
+let min, max; // declaring the variable here so i can acess it outside the function
+
+// Function to find min and max element within the numbers array
+function findMinMax() {
+  min = Math.min(...numbers);
+  max = Math.max(...numbers);
+}
+
+findMinMax(); // Call the function
+alert("The minimum element is: " + min + " and the maximum element is: " + max); // Display the result
