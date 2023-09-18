2ba01358dddf42e78a7a77aab853dd41d8e34b5e
Antoine Lansman <142655387+antoinel74@users.noreply.github.com>
2023-09-18 23:03:05 +0200

    Update 2.7.js

--- assets/scripts/2.7.js
+++ assets/scripts/2.7.js
@@ -1,8 +1,6 @@
 let n = parseInt(prompt("Enter a number :"));
 let sum = 0;
 
-// Step 3: Use a for loop to collect n numbers and calculate the sum 
-
 for (let i = 1; i <= n; i++) {
     let userInput = parseInt(prompt("Enter number :"));
     sum += userInput;
