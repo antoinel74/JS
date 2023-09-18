e27acc534a548a86c7a5bf4eb6b65af9ea186ec0
Antoine Lansman <142655387+antoinel74@users.noreply.github.com>
2023-09-18 23:02:24 +0200

    Update 1.1.js

--- assets/scripts/1.1.js
+++ assets/scripts/1.1.js
@@ -4,15 +4,3 @@ let name = "Antoine";
 let age = "26";
 
 console.log("My name is " + name + "and i got " + age); 
-
-// EXEMPLE SECTION // 
-
-let htmlcup = new Number('5');
-let csscup = new Number('8'); 
-let totalcup = htmlcup + csscup;
-
-console.log("Wow you got " + htmlcup + " html cup and " + csscup + " css cup, that's impressive !");
-console.log("You got " + totalcup + " cup in total !");
-
-console.log("ta mere" + " est gentille");
-console.log(5+15);
